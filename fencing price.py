30d3ebd9630b21d45b549324f3ecb2340e8d3e02
Toni-ggg <25toniguvo.acc@lwetb.ie>
2026-01-28 15:40:00 +0000

    Add files via upload

---
+++ fencing price.py
@@ -0,0 +1,9 @@
+rl = input('Enter the lenght of one side of the rectangle in metres : ')
+rl = float(rl)
+rw = input('Enter the width of one side of  the rectangle in metres: ')
+rw = float(rw)
+add = rl + rl + rw + rw
+price = input ('Enter the price per meter of fencing: ')
+price = float(price)
+multiply = add * price
+print('The cost to fence yor rectangle is: ',multiply)
