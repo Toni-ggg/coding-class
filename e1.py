bb12d52198b322bdadce95935ec8d1d9d74e91b5
Toni-ggg <25toniguvo.acc@lwetb.ie>
2026-02-28 20:34:42 +0000

    Add files via upload

---
+++ e1.py
@@ -0,0 +1,16 @@
+neg = input('Enter how much negatives you got: ')
+neg = int(neg)
+if neg == 1:
+     for i in range(11):
+         print('prompt')
+elif neg == 2:
+    for y in range(51):
+        print('reminder')
+elif neg == 3:
+    for u in range(101):
+        print('warning')
+else:
+    for q in range(501):
+        print('removal')
+   
+    
---
+++ e2.py
@@ -0,0 +1,8 @@
+age = input('Enter your age: ')
+age = int(age)
+if age > 12:
+    for i in range(101):
+        print('Happy Birthday')
+else:
+    for u in range(13):
+        print('Happy birthday')
