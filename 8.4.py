70eb09b647ca1f13604cbf695814d235eee1c3fb
Anthony Dilleen <adilleen.acc@lwetb.ie>
2026-05-06 14:14:09 +0100

    feedback on assignments 2 and 3

---
+++ 8.4.py
@@ -0,0 +1,4 @@
+radius = input('Enter the radius of your circle: ')
+radius= float(radius)
+area = 3.14 * radius * radius
+print('the area of your circle is: ',area)
---
+++ 8.5.py
@@ -0,0 +1,6 @@
+radius = input('Enter the radius of your cylinder: ')
+radius = float(radius)
+height = input('Enter the height of your cylinder: ')
+height = float(height)
+area= 3.14 * radius * radius * height
+print('The arera of your cylinder is',area,'units cubed')
